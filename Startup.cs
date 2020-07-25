8a847981a9f22511d4ee4f26d505efd7be77a784
Merino92 <curtidoilmb@gmail.com>
2020-07-24 22:34:36 -0600

    c

--- Startup.cs
+++ Startup.cs
@@ -78,7 +78,7 @@ namespace univo
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
 
             });
         }
