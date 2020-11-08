10e8ec4f9a659d997bc774e1766cd2deea3ac74d
Merino92 <curtidoilmb@gmail.com>
2020-11-07 21:44:09 -0600

    creando historial

---
+++ Controllers/HistorialController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace univo.Controllers
+{
+    public class HistorialController:Controller
+    {
+        public IActionResult Index()
+        {   
+            
+            return View();
+        }
+
+    }
+}
